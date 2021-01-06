b66bfe5d902e4204edfd5b5d6b830cd287c9fc88
PramodSaraswat <8441918081pramod@gmail.com>
2021-01-06 15:52:59 +0530

    completed rest

--- src/ModalView.jsx
+++ src/ModalView.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Card, Button, Form, Modal } from "react-bootstrap";
 
 const ModalView = (props) => {
 
-  const [localVal, setLocalVal] = useState("");
+  useEffect(() => {
+    setLocalVal(props.val || "")
+  }, [props.val]);
+
+  const [localVal, setLocalVal] = useState(props.val || "");
   const [validated, setValidated] = useState(false);
 
   const handleSubmit = (event) => {
@@ -11,8 +15,17 @@ const ModalView = (props) => {
     event.preventDefault();
     event.stopPropagation();
     if (form.checkValidity() === true) {
-      props.setValInParent(localVal);
-      setLocalVal("");
+      if (props.modalType === "addUser") {
+        props.setValInParent(localVal);
+        setLocalVal("");
+      } else if (props.modalType === "addTask") {
+        props.setValInParent(props.indexUser, localVal);
+        setLocalVal("");
+      } else if (props.modalType === "editUser") {
+        props.setValInParent(props.indexUser, localVal);
+      } else if (props.modalType === "editTask") {
+        props.setValInParent(props.indexUser[0], props.indexUser[1], localVal);
+      }
       props.toggleModal();
     }
     setValidated(true);
@@ -21,31 +34,53 @@ const ModalView = (props) => {
   const handleChange = (event) => {
     setLocalVal(event.target.value);
   };
-  return (<Modal
-        show={props.modalShow}
-        onHide={props.toggleModal}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
-          <Modal.Title className="text-center">{props.namVal}</Modal.Title>
-        </Modal.Header>
+
+  return (
+    <Modal
+      show={props.modalShow}
+      onHide={props.toggleModal}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Modal.Header closeButton>
+        <Modal.Title className="text-center">{props.namVal}</Modal.Title>
+      </Modal.Header>
+      
+      { !props.warn &&
         <Modal.Body>
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group>
-              <Form.Label>{props.labelName}</Form.Label>
-              <Form.Control placeholder={props.labelName} onChange={handleChange} name="val" value={localVal} required />
-              <Form.Control.Feedback type="invalid">
-              Required
-            </Form.Control.Feedback>
-            </Form.Group>
-            <Button variant="secondary" onClick={props.toggleModal}>
-              Close
-            </Button>
-            <Button variant="primary" className="ml-2" type="submit">Add</Button>
-          </Form>
-    </Modal.Body>
-      </Modal>);
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>{props.labelName}</Form.Label>
+            <Form.Control placeholder={props.labelName} 
+              onChange={handleChange} name="val" 
+              value={localVal} required 
+            />
+            <Form.Control.Feedback type="invalid">
+            Required
+          </Form.Control.Feedback>
+          </Form.Group>
+          <Button variant="secondary" onClick={props.toggleModal}>
+            Cancel
+          </Button>
+          <Button variant="primary" className="ml-2" type="submit">Save</Button>
+        </Form>
+        </Modal.Body>
+      }
+      { props.warn && 
+        <Modal.Footer>
+          <Button variant="secondary" onClick={props.toggleModal}>
+            No
+          </Button>
+          <Button variant="danger" className="ml-2" 
+            onClick={()=>props.deleteUser(props.indexUser)}
+          >
+            Yes
+           </Button>
+        </Modal.Footer>
+      }
+      
+    </Modal>
+  );
 }
 
 export default ModalView;
--- src/UserCard.js
+++ src/UserCard.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Card, Button } from "react-bootstrap";
 
 import ModalView from "./ModalView";
@@ -6,105 +6,190 @@ import ModalView from "./ModalView";
 
 const CardView = () => {
 
-	const [modalShow, setModalShow] = useState(false);
-	const [modalTaskShow, setModalTaskShow] = useState(false);
-	const [tasks, setTasks] = useState([]);
-	const [userData, setUserData] = useState({
-    data: [{
-        name: "aniket1",
-        tasks: ["task1", "task2" , "task3" ],
-      },
-      {
-        name: "aniket2",
-        tasks: ["task1", "task2" , "task3" ],
-      },
-      {
-        name: "aniket3",
-        tasks: ["task1", "task2" , "task3" ],
-      }
-    ]
-  })
+    const [modalShow, setModalShow] = useState(false);
+    const [modalTaskShow, setModalTaskShow] = useState(false);
+    const [warnShow, setWarnShow] = useState(false);
+    const [editUserShow, setEditUserShow] = useState(false);
+    const [editTaskShow, setEditTaskShow] = useState(false);
+    const [editNum, setEditNum] = useState(0);
+    const [editTaskNum, setEditTaskNum] = useState([0, 0]);
+    //provided mock data just for testing
+    const [userData, setUserData] = useState({
+      data: [{
+          name: "User1",
+          tasks: ["task1", "task2", "task3"],
+        },
+        {
+          name: "User2",
+          tasks: ["task1", "task2", "task3"],
+        },
+        {
+          name: "User3",
+          tasks: ["task1", "task2", "task3"],
+        }
+      ]
+    })
 
-	
+    const toggleModal = () => {
+      setModalShow(!modalShow);
+    }
 
-	const toggleModal = () => {
-		setModalShow(!modalShow);
-	}
+    const toggleTaskModal = () => {
+      setModalTaskShow(!modalTaskShow);
+    }
 
-	const toggleTaskModal = () => {
-		setModalTaskShow(!modalTaskShow);
-	}
+    const toggleWarnModal = () => {
+      setWarnShow(!warnShow);
+    }
 
-	const addUser = (userName) => {
-		console.log(userName)
-		let temp = userData.data
-		temp.push({
-					name: userName,
-					tasks: [],
-				})
-		setUserData({data: temp});
-		console.log(userData1);
-	}
+    const toggleUserShow = () => {
+      setEditUserShow(!editUserShow);
+    }
 
-	
-  const userData1 = {
-    data: [{
-        name: "aniket1",
-        tasks: ["task1", "task2" , "task3" ],
-      },
-      {
-        name: "aniket2",
-        tasks: ["task1", "task2" , "task3" ],
-      },
-      {
-        name: "aniket3",
-        tasks: ["task1", "task2" , "task3" ],
-      }
-    ]
-  }
+    const toggleEditTask = () => {
+      setEditTaskShow(!editTaskShow);
+    }
 
-  const deleteUser = (id) => {
-		setUserData({data:userData.data.filter((dat,index)=>{return(index !== id)})});
-		console.log(userData);	
-	}
+    const showAddTask = (id) => {
+      setEditNum(id);
+      toggleTaskModal();
+    }
 
-	const deleteTask = (userId,taskId) => {
-		let userDatas = userData.data;
-		let sUserData = userDatas[userId];
-		sUserData.tasks = sUserData.tasks.filter((taskDat,taskInd)=>{return(taskInd !== taskId)});
-		userDatas[userId] = sUserData;
-		console.log(userDatas);
-		setUserData({data:userDatas});
-	}
+    const showDelUser = (id) => {
+      setEditNum(id);
+      toggleWarnModal();
+    }
 
-  return (<>
-  	<div style={{width:"100%",float:"right"}}>
-  		<Button className="btn btn-primary m-4" style={{float:"right"}} onClick={toggleModal}>
-  			Add User
-  		</Button>
-  	</div>
-    <div className="row">
-		{userData.data.map(({ name, tasks },indexUser) =>
-		          	<div className="m-4 mx-auto col-12 col-sm-6 col-lg-3" key={indexUser}>
+    const showUserEdit = (ind) => {
+      setEditNum(ind);
+      toggleUserShow();
+    }
+
+    const showTaskEdit = (ind, taskInd) => {
+      setEditTaskNum([ind, taskInd]);
+      toggleEditTask();
+    }
+
+    const addUser = (userName) => {
+      console.log(userName)
+      let temp = userData.data
+      temp.push({
+        name: userName,
+        tasks: [],
+      })
+      setUserData({ data: temp });
+    }
+
+
+    const deleteUser = (id) => {
+      setUserData({ data: userData.data.filter((dat, index) => { return (index !== id) }) });
+      setEditNum(0);
+      toggleWarnModal();
+    }
+
+    const editUserName = (userId, userName) => {
+      let userDatas = userData.data;
+      userDatas[userId].name = userName;
+      setUserData({ data: userDatas });
+    }
+
+    const addTask = (userId, task) => {
+      let userDatas = userData.data;
+      userDatas[userId].tasks.push(task);
+      setUserData({ data: userDatas });
+    }
+
+    const editTask = (userId, taskId, val) => {
+      let userDatas = userData.data;
+      userDatas[userId].tasks[taskId] = val;
+      setUserData({ data: userDatas });
+    }
+
+    const deleteTask = (userId, taskId) => {
+      let userDatas = userData.data;
+      userDatas[userId].tasks = userDatas[userId].tasks.filter((taskDat, taskInd) => { return (taskInd !== taskId) });
+      setUserData({ data: userDatas });
+      setEditTaskNum([0,0])
+    }
+
+    return ( <>
+	    <div style={{width:"100%",float:"right"}}>
+	  		<Button className="btn btn-primary m-4" 
+	  			style={{float:"right"}} 
+	  			onClick={toggleModal}
+	  		>
+	  			Add User
+	  		</Button>
+  		</div> 
+  		<div className = "row mr-0" > 
+  			{userData.data.map(({ name, tasks }, indexUser) =>
+          		<div className="m-4 mx-auto col-12 col-sm-6 col-lg-3" key={indexUser}>
 		            <Card style={{ width: 'auto' }} >
-		                <Card.Title  className="mb-2 mt-2 text-center" variant="top">{name} 
-		                <Button className="btn btn-danger mr-2" style={{position:"absolute",right:0}} onClick={()=>deleteUser(indexUser)}>X</Button>
+		                <Card.Title  className="mb-2 mt-2 text-center" variant="top">
+		                	<p className="d-inline" style={{cursor:"pointer"}} 
+		                		onClick={()=>showUserEdit(indexUser)}
+		                	>
+		                		{name}
+		                	</p> 
+			                <Button className="btn btn-danger mr-2" 
+			                	style={{position:"absolute",right:0}} 
+			                	onClick={()=>showDelUser(indexUser)}
+			                >
+			                	X
+			                </Button>
+			                <ModalView modalShow={editUserShow} modalType={"editUser"} 
+			                	val={userData.data[editNum].name} toggleModal={toggleUserShow} 
+			                	namVal={"Edit user name"} indexUser={editNum} 
+			                	setValInParent={editUserName} 
+			                />
+			                <ModalView modalShow={warnShow} warn={true} 
+			                	toggleModal={toggleWarnModal} namVal={"Confirm delete user?"} 
+			                	indexUser={editNum} deleteUser={deleteUser} 
+			                />
 		                </Card.Title>
 		                <hr />
-		  <Card.Body>
-		    {tasks.map((taskDesc,indexTask)=>{
-		    	return(<p key={indexTask}><b className="mr-2 btn btn-danger" style={{borderRadius:"50%",padding: "0rem 0.4rem"}} onClick={()=>deleteTask(indexUser,indexTask)}>X</b>{taskDesc}</p>);
-		    })}
-		    <div className="text-center">
-		    	<Button variant="primary" onClick={toggleTaskModal}>+</Button>
-		    </div>
-		  </Card.Body>
-		</Card></div>)}
-	</div>
-	<ModalView modalShow={modalShow} toggleModal={toggleModal} namVal={"Add User"} labelName={"Enter Username"} setValInParent={addUser} />
-	<ModalView modalShow={modalTaskShow} toggleModal={toggleTaskModal} namVal={"Add Task"} labelName={"Enter Task"} />
+		  				<Card.Body>
+		    				{tasks.map((taskDesc,indexTask)=>{
+		    					return(
+		    						<p key={indexTask}>
+			    						<b className="mr-2 btn btn-danger" 
+			    							style={{borderRadius:"50%",padding: "0rem 0.4rem"}} 
+			    							onClick={()=>deleteTask(indexUser,indexTask)}
+			    						>
+			    							X
+			    						</b>
+			    						<b onClick={()=>showTaskEdit(indexUser,indexTask)} 
+			    							style={{cursor:"pointer"}}
+			    						>
+			    							{taskDesc}
+			    						</b>
+		    						</p>
+		    					);
+		    				})}
+		    				<div className="text-center">
+		    					<Button variant="primary" onClick={()=>showAddTask(indexUser)}
+		    					>
+		    						+
+		    					</Button>
+			    				<ModalView modalShow={modalTaskShow} modalType={"addTask"} 
+			    					toggleModal={toggleTaskModal} namVal={"Add Task"} labelName={"Enter Task"} 
+			    					indexUser={editNum} setValInParent={addTask} 
+			    				/>
+			    				<ModalView modalShow={editTaskShow} modalType={"editTask"} 
+			    					val={userData.data[editTaskNum[0]].tasks[editTaskNum[1]]} 
+			    					toggleModal={toggleEditTask} namVal={"Edit Task"} labelName={"Enter Task"} 
+			    					setValInParent={editTask} indexUser={editTaskNum} 
+			    				/>
+		    				</div>
+		  				</Card.Body>
+					</Card>
+				</div>
+			)} 
+		</div> 
+		<ModalView modalShow = { modalShow } modalType = { "addUser" } toggleModal = { toggleModal } 
+			namVal = { "Add User" } labelName = { "Enter Username" } setValInParent = { addUser }
+		/> 
 	</>);
-
 }
 
 export default CardView;
